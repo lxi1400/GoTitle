5cdebb5886ec56393cfed61f577ae3c28dcf1931
lxi <75556084+lxi1400@users.noreply.github.com>
2021-03-14 01:52:05 -0600

    Add files via upload

---
+++ title.go
@@ -0,0 +1,20 @@
+package title
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func SetTitle(title string) (int, error) {
+	handle, err := syscall.LoadLibrary("Kernel32.dll")
+	if err != nil {
+		return 0, err
+	}
+	defer syscall.FreeLibrary(handle)
+	proc, err := syscall.GetProcAddress(handle, "SetConsoleTitleW")
+	if err != nil {
+		return 0, err
+	}
+	r, _, err := syscall.Syscall(proc, 1, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))), 0, 0)
+	return int(r), err
+}
